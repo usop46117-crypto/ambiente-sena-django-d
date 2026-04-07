49ed5747c2206e2e0f5785d481dc4328c23cf0d0
HallangRuss <hallangruss988@gmail.com>
2026-04-07 10:42:29 -0500

    Estructura final para despliegue con Procfile

--- ambienteSena/AmbienteSena/build.sh
+++ ambienteSena/AmbienteSena/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
 
+# Recolectar archivos estáticos (CSS/JS)
 python manage.py collectstatic --no-input
 
+# Aplicar migraciones de base de datos
 python manage.py migrate
